47f3ab0f1c9156a092b0a9a623f3a8ba12a7a298
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2020-06-05 14:46:12 +0530

    Added login interface which receive code from github oauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  STATUS,
+  Loading,
+  Avatar,
+  Logo,
+  Logotype,
+  Container,
+  Header
+} from "gitstar-components";
+
+const CLIENT_ID = "8b508ba452a263f604b4";
+const REDIRECT_URI = "http://localhost:3000/";
+
+class App extends Component {
+  state = {
+    status: STATUS.INITIAL,
+    token: null
+  };
+
+  componentDidMount() {
+    // const code =
+    //   window.location.href.match(/?code=(.*)/) &&
+    //   window.location.href.match(/?code=(.*)/)[1];
+    let code = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('code').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+    if (code) {
+      console.log(code);
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Logo />
+            <Logotype />
+          </div>
+          <a
+            href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+          >
+            Login
+          </a>
+        </Header>
+      </Container>
+    );
+  }
 }
 
 export default App;

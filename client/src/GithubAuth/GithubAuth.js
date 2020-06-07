ab34e7d0704b51d6fe2c89d8ab67e999c70f0228
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2020-06-07 08:08:07 +0530

    Added the mutation to send code and get token

--- client/src/GithubAuth/GithubAuth.js
+++ client/src/GithubAuth/GithubAuth.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
 
 import {
     STATUS,
@@ -8,6 +10,16 @@ import {
     Header
 } from "gitstar-components";
 
+const getToken = gql`
+mutation($code: String!){
+    GithubAuth(code: $code){
+        status
+        message
+        token
+    }
+}
+`;
+
 const CLIENT_ID = "37454df5e11a69f88833";
 const REDIRECT_URI = "http://localhost:3000/";
 
@@ -16,15 +28,8 @@ class GithubAuth extends Component {
         status: STATUS.INITIAL,
         token: null,
         isLoggedIn: false,
+        code: null,
     };
-
-    componentDidMount() {
-        let code = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('code').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-        if (code) {
-            console.log(code);
-            this.setState({ isLoggedIn: true })
-        }
-    }
 
     render() {
 
@@ -33,20 +38,35 @@ class GithubAuth extends Component {
             loggedIn = <p>You are Logged In</p>
         }
 
-        return (
-            <Container>
-                <Header>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                        <Logo />
-                        <Logotype />
-                    </div>
-                    <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
-                        Login
-                    </a>
-                </Header>
-                {loggedIn}
-            </Container>
-        );
+        let display = <Container>
+            <Header>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <Logo />
+                    <Logotype />
+                </div>
+                <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
+                    Login
+            </a>
+            </Header>
+            {loggedIn}
+        </Container>
+
+        let code = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('code').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+        if (code) {
+            console.log(code);
+            this.setState({ isLoggedIn: true });
+            display = <Mutation mutation={getToken} variables={{ code }}>
+                {(GithubAuth, { data }) => {
+                    GithubAuth(code);
+                    this.setState({ token: data.GithubAuth.token })
+                    return <p>{data.GithubAuth.token}</p>
+                }}
+            </Mutation>
+        }
+
+
+
+        return display;
     }
 }
 

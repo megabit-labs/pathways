79cb3be445dd2fac60d5d51390a3c2c9caa937ae
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2020-06-07 07:32:12 +0530

    added the graphql mutation to send the code and retrieve token

--- client/src/GithubAuth/GithubAuth.js
+++ client/src/GithubAuth/GithubAuth.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
 
 import {
     STATUS,
@@ -8,6 +11,16 @@ import {
     Header
 } from "gitstar-components";
 
+const getToken = gql`
+            mutation($code: String!){
+                GithubAuth(code: $code){
+                    status
+                    message
+                    token
+                }
+            }
+            `;
+
 const CLIENT_ID = "8b508ba452a263f604b4";
 const REDIRECT_URI = "http://localhost:3000/";
 
@@ -16,37 +29,49 @@ class GithubAuth extends Component {
         status: STATUS.INITIAL,
         token: null,
         isLoggedIn: false,
+        code: null,
     };
 
-    componentDidMount() {
-        let code = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('code').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-        if (code) {
-            console.log(code);
-            this.setState({ isLoggedIn: true })
+    fetchToken = () => {
+        let code = this.state.code;
+        if (code !== null) {
+            this.props.getToken({
+                variables: {
+                    code: code
+                }
+            });
         }
     }
 
     render() {
 
-        let loggedIn = null;
-        if (this.state.isLoggedIn) {
-            loggedIn = <p>You are Logged In</p>
-        }
+        let display = <Container>
+            <Header>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <Logo />
+                    <Logotype />
+                </div>
+                <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
+                    Login
+            </a>
+            </Header>
+        </Container>
 
+        let code = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('code').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+        if (code) {
+            console.log(code);
+            this.setState({ code: code });
+            display = <Mutation mutation={getToken} variables={{ code }}>
+                {(GithubAuth, { data }) => {
+                    GithubAuth();
+                    this.setState({ token: data.GithubAuth.token })
+                    return <p>{data.GithubAuth.token}</p>
+                }}
+            </Mutation>
+        }
         return (
-            <Container>
-                <Header>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                        <Logo />
-                        <Logotype />
-                    </div>
-                    <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
-                        Login
-                    </a>
-                </Header>
-                {loggedIn}
-            </Container>
-        );
+            display
+        )
     }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 serviceWorker.unregister();

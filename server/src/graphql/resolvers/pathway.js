ec334effaf60e910f0af95cf27f114270faf877d
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-06-08 10:57:38 +0530

    Add API to modify pathway
    Wiki entry: https://github.com/dush-t/pathways/wiki/Schema-design-for-modifying-a-pathway

--- server/src/graphql/resolvers/pathway.js
+++ server/src/graphql/resolvers/pathway.js
@@ -15,6 +15,20 @@ const resolver = {
             }
 
             return { status: 'OK', message: null }
+        },
+        async modifyPathwaySteps(_, {id, createSteps, updateSteps, deleteSteps}) {
+            const query = queries.pathway.modifyPathwaySteps({
+                id, createSteps, updateSteps, deleteSteps
+            })
+
+            try {
+                await query.run()
+                return { status: 'OK', message: null }
+            } catch (e) {
+                return { status: 'ERROR', message: e.toString() }
+            }
+
+            return { status: 'OK', message: null }
         }
     }
 }

17588ebbc3fac9e11045c9ac5e0fdc1bc5a42a41
Dushyant Yadav <dushyant9309@gmail.com>
2020-06-22 18:20:37 +0530

    Listen for ctrl+s events when editing a pathway

--- client/src/screens/CreateEditPathway/CreateEditPathway.js
+++ client/src/screens/CreateEditPathway/CreateEditPathway.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-import StepDndList from '../../components/StepDndList/stepDndList'
-import StepEditArea from '../../components/StepEditArea/StepEditArea'
+import StepDndList from "../../components/StepDndList/stepDndList"
+import StepEditArea from "../../components/StepEditArea/StepEditArea"
 
-import classes from './CreateEditPathway.module.css'
-
+import classes from "./CreateEditPathway.module.css"
 
 class CreateEditPathway extends Component {
+
+    componentDidMount() {
+        window.addEventListener('keydown', (e) => this.handleKeyDown(e))
+    }
+
+    handleKeyDown = (event) => {
+        let charCode = String.fromCharCode(event.which).toLowerCase()
+        if ((event.ctrlKey || event.metaKey) && charCode === 's') {
+            event.preventDefault()
+            console.log('User tried to save')
+        }
+    }
+
     render() {
         return (
-            <div className={classes.Content}>
+            <div 
+                className={classes.Content}
+                onKeyDown={this.handleKeyDown}
+            >
                 <div className={classes.EditArea}>
                     <StepEditArea />
                 </div>

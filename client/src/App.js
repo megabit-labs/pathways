f5ff49f6dffd968871742851bb6be0b1f7148516
Shreyans Jain <shreyansj551@gmail.com>
2020-07-09 11:47:59 +0530

    resolve app.js merge coflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from "react";
 
-<<<<<<< HEAD
 import GithubAuth from './components/GithubAuth/GithubAuth';
 import StepDnDList from './components/StepDndList/stepDndList'
 import CreateEditPathway from './screens/CreateEditPathway/CreateEditPathway'
-=======
-import SearchPage from './components/SearchPage/SearchPage';
->>>>>>> master
 
 class App extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       // <GithubAuth />
       // <StepDnDList />
       <CreateEditPathway />
-=======
-      <SearchPage />
->>>>>>> master
     );
   }
 }

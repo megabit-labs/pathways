915da1761bcab8d214bc861e8686733ba1eb4b3d
kavya <kavya.shree@venuemonk.com>
2020-07-10 17:44:44 +0530

    add the login with github button

--- client/src/components/SearchPage/Header.js
+++ client/src/components/SearchPage/Header.js
@@ -1,30 +1,30 @@
-import React, {Component} from 'react';
-import {Navbar, Nav, NavLink} from 'react-bootstrap';
-import classes from './Header.module.css'
-import Menu from '../assets/menu.png'
-import Icon from  '../assets/icon.png' 
-   
-class Header extends Component{
+import React, { Component } from "react"
+import { Navbar, Nav, NavLink, Button } from "react-bootstrap"
+import classes from "./Header.module.css"
+import Menu from "../assets/menu.png"
 
+class Header extends Component {
     render() {
-        return(
-<React.Fragment>
-<Navbar className={classes.navbar}>
-    <Nav className={classes.navlinks}>
-        <Nav.Link href="#">
-            <img
-                src={Menu}
-                className={classes.apps}/>
-        </Nav.Link>
-        <Nav.Link href="#">
-            <img
-                src={Icon}
-                className={classes.dp}/>
-        </Nav.Link>
-    </Nav>
-</Navbar>
-</React.Fragment>
-        );
-    };
-};
-export default Header;
+        return (
+            <React.Fragment>
+                <Navbar className={classes.navbar}>
+                    <Nav className={classes.navlinks}>
+                        <Nav.Link href="#">
+                            <img src={Menu} className={classes.apps} />
+                        </Nav.Link>
+                        <Nav.Link href="#">
+                            <button className={classes.login}>
+                                <img
+                                    className={classes.github}
+                                    src="https://ik.imagekit.io/m52sq26n4h/icons8-github-48__1_.png"
+                                />
+                               <text className={classes.text}>Login with GitHub</text> 
+                            </button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar>
+            </React.Fragment>
+        )
+    }
+}
+export default Header

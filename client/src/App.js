271b6f6c40dfad092dc3ed6ebeb746a43834681b
Dushyant Yadav <dushyant9309@gmail.com>
2020-07-14 15:55:11 +0530

    The bugfix of shame

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react"
 import { Route, Switch } from "react-router-dom"
 
+import GithubAuth from './components/GithubAuth/GithubAuth'
 import CreateEditPathway from './screens/CreateEditPathway/CreateEditPathway'
 import SearchPage from "./components/SearchPage/SearchPage"
 

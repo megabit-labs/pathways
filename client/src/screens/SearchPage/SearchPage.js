b5e468d05e7d2de8d17f1fedd5cf106639ac2990
kavya <kavya.shree@venuemonk.com>
2020-07-15 17:40:07 +0530

    redo refactoring changes

--- client/src/screens/SearchPage/SearchPage.js
+++ client/src/screens/SearchPage/SearchPage.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Header from "./Header"
-import SearchField from "./SearchField"
-import Footer from "./Footer"
+import Header from "../../components/SearchPage/Header/Header"
+import SearchField from "../../components/SearchPage/SearchField/SearchField"
+import Footer from "../../components/SearchPage/Footer/Footer"
 
 const searchPage = (props) => {
 

fdeaaad8eedcaa9fee8264c31e8ab6195e8c1582
Tirth Jain <jaintirth24@gmail.com>
2020-08-07 02:12:05 +0530

    Search hotfix

--- server/src/db/queries/pathway/search.js
+++ server/src/db/queries/pathway/search.js
@@ -1,17 +1,16 @@
 const Query = require('../../core/Query')
-const { NEO4J_INDEX_NAME } = require('../../../../config')
+const { NEO4J_SEARCH_INDEX } = require('../../../../config')
 
 const search = ({ searchQuery }) => {
-    return query = new Query({
+    return new Query({
         statement: `
             CALL db.index.fulltext.queryNodes($indexName, $searchQuery)
             YIELD node, score
             MATCH (node)
             RETURN node.label as typem, node.id as id, score ORDER BY score DESC
         `,
-        params: { indexName: NEO4J_INDEX_NAME, searchQuery }
+        params: { indexName: NEO4J_SEARCH_INDEX, searchQuery },
     })
 }
 
 module.exports = search
-

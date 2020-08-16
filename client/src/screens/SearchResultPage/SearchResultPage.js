b4ccdca49b51767d45e8e22f028d38916377b85c
kavya <kavya.shree@venuemonk.com>
2020-08-16 15:26:48 +0530

    dynamic text

--- client/src/screens/SearchResultPage/SearchResultPage.js
+++ client/src/screens/SearchResultPage/SearchResultPage.js
@@ -56,10 +56,7 @@ export default function SearchResultPage({ match, props }) {
                             </div>
                             <div className={classes.text}>
                                 <p className={classes.description}>
-                                    A standard dummy text ever since the 1500s
-                                    when an unknown printer took a galley of
-                                    type and scrambled it to make a type
-                                    specimen book it has?
+                                    {item.description}
                                 </p>
                             </div>
                         </div>

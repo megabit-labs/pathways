e482d2122c9f279f9e6e417dd205f8d1f2ae0a01
Tirth Jain <jaintirth24@gmail.com>
2020-12-26 15:20:51 +0530

    Update config.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 // Github Oauth. 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || "8b508ba452a263f604b4"
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || "16ef48033babee3d5325c03c50389ec8eed087a7"
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || "37454df5e11a69f88833"
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || "a92ad05b01bdf4243113f940b62c8c20e8dff691"
 
 // Github owner and repo for pathway contents
 const GITHUB_OWNER = process.env.GITHUB_OWNER || "megabit-labs"
